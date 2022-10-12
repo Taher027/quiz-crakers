e517f192b614b3d4b868d2dc5c06965ffc9b71da
Taher027 <abutaherh27@gmail.com>
2022-10-12 20:33:22 +0600

    added some css class

--- src/components/Quiz/QuizDetails.js
+++ src/components/Quiz/QuizDetails.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import img from '../../assets/eyes.png'
 
 const QuizDetails = ({ SingleQuestion }) => {
     // console.log(SingleQuestion)
     const { question, options, correctAnswer } = SingleQuestion;
 
     return (
-        <div>
-            <div className='border m-5 p-5'>
-                <h2 className='text-2xl font-semibold'>{question}</h2>
+        <div className='w-9/12 mx-auto '>
+            <div className='border m-5 p-5 shadow-md'>
+                <div className='grid sm: grid-cols-none md:grid-cols-2 justify-end'>
+                    <h2 className='text-1xl font-semibold'>{question}</h2>
+                    <img  src={img} alt="" />
+                </div>
                 <p className='mt-2'>Answer:</p>
                 <div className=''>
                     <div> <input className='mx-6' type="radio" name='option' />{options[0]} </div>
                     <div> <input className='mx-6' type="radio" name='option' />{options[1]} </div>
-                    <div> <input className='mx-6' type="radio" name='option' />{options[2]} </div> 
+                    <div> <input className='mx-6' type="radio" name='option' />{options[2]} </div>
                     <div> <input className='mx-6' type="radio" name='option' />{options[3]} </div>
                 </div>
 
